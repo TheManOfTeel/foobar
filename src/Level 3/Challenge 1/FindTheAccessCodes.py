76c68c129a601067e39a7ba02000e37e4005c25b
Danny <themanofteel@icloud.com>
2021-06-20 18:47:58 -0400

    solution to find the access codes

--- src/Level 3/Challenge 1/FindTheAccessCodes.py
+++ src/Level 3/Challenge 1/FindTheAccessCodes.py
@@ -1,9 +1,18 @@
+# Find the number of luckky triples in the list
 def solution(l):
-    return 0
+    c = [0] * len(l)
+    count = 0
+    for i in range(0,len(l)):
+        j = 0
+        for j in range(0, i):
+            if l[i] % l[j] == 0:
+                c[i] = c[i] + 1
+                count = count + c[j]
+    return count
 
 # Test function
 def main():
-    print(solution())
+    print(solution([1, 2, 3, 4, 5, 6]))
 
 # Run test
 main()
