7dbf752ee5f5f975d4768e7336dec9c155d01dad
Danny <themanofteel@icloud.com>
2021-06-20 19:15:49 -0400

    bomb baby solution

--- src/Level 3/Challenge 2/BombBaby.py
+++ src/Level 3/Challenge 2/BombBaby.py
@@ -1,10 +1,20 @@
 # Find the number of bomb replication cycles needed for the correct amount of bombs
-def solution(l):
-    return 0
+def solution(x, y):
+    x, y = int(x), int(y)
+    cycles = 0
+
+    while (x != 1 and y != 1):
+        if x % y == 0:
+            return "impossible"
+        else :
+            cycles = cycles + int(max(x, y) / min(x, y))
+            x, y = max(x, y) % min(x, y), min(x, y)
+
+    return str(cycles + max(x, y) - 1)
 
 # Test function
 def main():
-    print(solution([]))
+    print(solution('4', '7'))
 
 # Run test
 main()
