f241595e9ac766c54f6f1860695a2d1395516319
Danny <themanofteel@icloud.com>
2021-06-20 18:58:58 -0400

    bomb baby question

---
+++ src/Level 3/Challenge 2/BombBaby.py
@@ -0,0 +1,10 @@
+# Find the number of bomb replication cycles needed for the correct amount of bombs
+def solution(l):
+    return 0
+
+# Test function
+def main():
+    print(solution([]))
+
+# Run test
+main()
